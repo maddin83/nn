f73d210842cf529e09154d86b8d44481bcf1f286
Martin <martin.schmidt@zalando.de>
2016-05-21 00:05:16 +0200

    initial test nn

---
+++ src/main/java/de/neuronal/stock/NeuronalApplication.java
@@ -0,0 +1,13 @@
+package de.neuronal.stock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeuronalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeuronalApplication.class, args);
+		System.out.println("LALA");
+	}
+}
---
+++ src/main/java/de/neuronal/stock/controller/NeuronalController.java
@@ -0,0 +1,86 @@
+package de.neuronal.stock.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.neuroph.core.Layer;
+import org.neuroph.core.NeuralNetwork;
+import org.neuroph.core.events.LearningEvent;
+import org.neuroph.core.events.LearningEventListener;
+import org.neuroph.nnet.comp.neuron.BiasNeuron;
+import org.neuroph.nnet.learning.MomentumBackpropagation;
+import org.neuroph.util.ConnectionFactory;
+import org.neuroph.util.LayerFactory;
+import org.neuroph.util.NeuralNetworkFactory;
+import org.neuroph.util.NeuralNetworkType;
+import org.neuroph.util.NeuronProperties;
+import org.neuroph.util.TransferFunctionType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@RestController
+public class NeuronalController {
+	
+	private NeuralNetwork myMlPerceptron = new NeuralNetwork();
+	
+	@RequestMapping("/initiate")
+    public int initiate() {
+		
+		NeuronProperties neuronProperties = new NeuronProperties();
+	    neuronProperties.setProperty("transferFunction", TransferFunctionType.SIGMOID);
+
+		// create multi layer perceptron
+	    myMlPerceptron.setNetworkType(NeuralNetworkType.MULTI_LAYER_PERCEPTRON);
+
+	    // create input layer
+	    Layer inputLayer = LayerFactory.createLayer(2, neuronProperties);
+	    inputLayer.addNeuron(new BiasNeuron());
+	    myMlPerceptron.addLayer(inputLayer);
+	    
+	    // create hidden layer 1
+	    Layer hiddenLayer1 = LayerFactory.createLayer(9, neuronProperties);
+	    hiddenLayer1.addNeuron(new BiasNeuron());
+	    myMlPerceptron.addLayer(hiddenLayer1);
+
+	    // create full conectivity between input and hidden layer 1
+	    ConnectionFactory.fullConnect(inputLayer, hiddenLayer1);
+	 // createLayer output layer
+	    Layer outputLayer = LayerFactory.createLayer(1, neuronProperties);
+	    myMlPerceptron.addLayer(outputLayer);
+
+	    // create full conectivity between input and hidden layer 2
+	    ConnectionFactory.fullConnect(hiddenLayer1, outputLayer);
+
+	    // set input and output cells for network
+	    NeuralNetworkFactory.setDefaultIO(myMlPerceptron);
+
+	    int maxIterations = 1000000;
+
+	    double learningRate = 0.2;
+
+	    double maxError = 0.000001;
+	    double momentum = 0.7;
+
+	    MomentumBackpropagation learningRule = new MomentumBackpropagation();
+	    myMlPerceptron.setLearningRule(learningRule);
+
+	    learningRule.setMomentum(momentum);
+	    learningRule.setMaxError(maxError);
+	    learningRule.setLearningRate(learningRate);
+	    learningRule.setMaxIterations(maxIterations);
+
+	    learningRule.addListener(new LearningEventListener() {
+	        public void handleLearningEvent(LearningEvent learningEvent){
+	            MomentumBackpropagation rule = (MomentumBackpropagation) learningEvent.getSource();
+
+	        }
+	    });
+
+	    return myMlPerceptron.getLayersCount();  }
+	
+	@RequestMapping("/test")
+    public double[] test(@RequestParam(value="in1", defaultValue="1") int in1, @RequestParam(value="in2", defaultValue="1") int in2) {
+		//myMlPerceptron.setInput();
+		myMlPerceptron.calculate();
+		return myMlPerceptron.getOutput();
+	}
+	
+}
